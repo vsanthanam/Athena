9b1f9a10b5256dd18220e7dbda4044a3ae289516
Varun Santhanam <talkto@vsanthanam.com>
2023-04-30 19:51:23 -0700

    Update Static Site Generator

--- generate-static-site.sh
+++ generate-static-site.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-if [[ "$branch" != "main" ]]; then
-    echo "Invalid Branch"
-else
+run_docc () {
+   swift package --allow-writing-to-directory docs generate-documentation --target Athena --disable-indexing --transform-for-static-hosting --hosting-base-path Athena/docs --output-path docs
+}
+
+create_branches () {
     git branch -D gh-pages
     git checkout -b gh-pages
-    rm -rf ~/Library/Developer/Xcode/DerivedData
-    xcodebuild docbuild -scheme Athena \
-    -destination generic/platform=iOS \
-    OTHER_DOCC_FLAGS="--include-extended-types --transform-for-static-hosting --hosting-base-path Athena/docs --output-path docs"
+}
+
+fix_readme () {
     tail -n +2 README.md > README-2.md && mv README-2.md README.md
+}
+
+commit_and_publish () {
     git add .
     git commit -m 'Synchronize Hompage & Publish Documentation'
     git push -f -u origin gh-pages
+}
+
+clean_up () {
     git checkout main
     git branch -D gh-pages
     rm -rf docs
+}
+
+generate_documentation () {
+    create_branches
+    run_docc
+    fix_readme
+    commit_and_publish
+    clean_up
+}
+
+if [[ "$branch" != "main" ]]; then
+    echo "Invalid Branch"
+elif [[ -n $(git status -s) ]]; then
+    echo "Uncommited Changes"
+else
+    generate_documentation
     echo "Website Updated!"
 fi
